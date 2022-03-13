42d9ebaabc18c68d6a1afc9112e5ff8315e87742
David Sanders <david.sanders@postman.com>
2022-03-12 23:11:53 -0800

    feat: improve customizability of `e cherry-pick`

--- src/e-cherry-pick.js
+++ src/e-cherry-pick.js
@@ -3,6 +3,8 @@
 const d = require('debug')('build-tools:cherry-pick');
 const program = require('commander');
 const https = require('https');
+const open = require('open');
+const querystring = require('querystring');
 const { Octokit } = require('@octokit/rest');
 
 const evmConfig = require('./evm-config');
@@ -29,185 +31,206 @@ function fetchBase64(url) {
 
 program
   .arguments('<patch-url> <target-branch> [additionalBranches...]')
+  .option('-i|--interactive', 'Whether to automatically create the PR, or open in a browser')
+  .option('-m|--message <message>', 'Commit message')
+  .option('--patch-name <name>', 'Patch name')
   .option('--security', 'Whether this backport is for security reasons')
   .description('Opens a PR to electron/electron that backport the given CL into our patches folder')
   .allowUnknownOption(false)
-  .action(async (patchUrlStr, targetBranch, additionalBranches, { security }) => {
-    const forkUsername = evmConfig.getForkUsername(evmConfig.current());
-    const owner = forkUsername ? forkUsername : 'electron';
+  .action(
+    async (
+      patchUrlStr,
+      targetBranch,
+      additionalBranches,
+      { interactive, message, patchName, security },
+    ) => {
+      const forkUsername = evmConfig.getForkUsername(evmConfig.current());
+      const owner = forkUsername ? forkUsername : 'electron';
 
-    if (targetBranch.startsWith('https://')) {
-      let tmp = patchUrlStr;
-      patchUrlStr = targetBranch;
-      targetBranch = tmp;
-    }
-    const octokit = new Octokit({
-      auth: process.env.ELECTRON_BUILD_TOOLS_GH_AUTH || (await getGitHubAuthToken(['repo'])),
-    });
-    try {
-      const {
-        data: { permissions },
-      } = await octokit.repos.get({
-        owner,
-        repo: 'electron',
+      if (targetBranch.startsWith('https://')) {
+        let tmp = patchUrlStr;
+        patchUrlStr = targetBranch;
+        targetBranch = tmp;
+      }
+      const octokit = new Octokit({
+        auth: process.env.ELECTRON_BUILD_TOOLS_GH_AUTH || (await getGitHubAuthToken(['repo'])),
       });
-      if (!permissions || !permissions.push) {
-        fatal(
-          'The supplied $GITHUB_TOKEN does not have write access to electron/electron - exiting',
-        );
-      }
-
-      const targetBranches = [targetBranch, ...additionalBranches];
-
-      const gerritUrl = new URL(patchUrlStr);
-      if (
-        gerritUrl.host !== 'chromium-review.googlesource.com' &&
-        gerritUrl.host !== 'skia-review.googlesource.com'
-      ) {
-        fatal(
-          'Expected a gerrit URL (e.g. https://chromium-review.googlesource.com/c/v8/v8/+/2465830)',
-        );
-      }
-      const [, repo, number] = /^\/c\/(.+?)\/\+\/(\d+)/.exec(gerritUrl.pathname);
-
-      d(`fetching patch from gerrit`);
-      const changeId = `${repo}~${number}`;
-      const patchUrl = new URL(
-        `/changes/${encodeURIComponent(changeId)}/revisions/current/patch`,
-        gerritUrl,
-      );
-      const patch = await fetchBase64(patchUrl.toString());
-
-      const [, commitId] = /^From ([0-9a-f]+)/.exec(patch);
-
-      const patchDirName =
-        {
-          'chromium/src': 'chromium',
-          skia: 'skia',
-        }[repo] || repo.split('/')[1];
-
-      const shortCommit = commitId.substr(0, 12);
-      const patchName = `cherry-pick-${shortCommit}.patch`;
-      const patchPath = `patches/${patchDirName}`;
-      for (const target of targetBranches) {
-        const branchName = `cherry-pick/${target}/${patchDirName}/${shortCommit}`;
-        d(`fetching electron base branch info for ${target}`);
+      try {
         const {
-          data: {
-            commit: {
-              sha: targetSha,
+          data: { permissions },
+        } = await octokit.repos.get({
+          owner,
+          repo: 'electron',
+        });
+        if (!permissions || !permissions.push) {
+          fatal(
+            'The supplied $GITHUB_TOKEN does not have write access to electron/electron - exiting',
+          );
+        }
+
+        const targetBranches = [targetBranch, ...additionalBranches];
+
+        const gerritUrl = new URL(patchUrlStr);
+        if (
+          gerritUrl.host !== 'chromium-review.googlesource.com' &&
+          gerritUrl.host !== 'skia-review.googlesource.com'
+        ) {
+          fatal(
+            'Expected a gerrit URL (e.g. https://chromium-review.googlesource.com/c/v8/v8/+/2465830)',
+          );
+        }
+        const [, repo, number] = /^\/c\/(.+?)\/\+\/(\d+)/.exec(gerritUrl.pathname);
+
+        d(`fetching patch from gerrit`);
+        const changeId = `${repo}~${number}`;
+        const patchUrl = new URL(
+          `/changes/${encodeURIComponent(changeId)}/revisions/current/patch`,
+          gerritUrl,
+        );
+        const patch = await fetchBase64(patchUrl.toString());
+
+        const [, commitId] = /^From ([0-9a-f]+)/.exec(patch);
+
+        const patchDirName =
+          {
+            'chromium/src': 'chromium',
+            skia: 'skia',
+          }[repo] || repo.split('/')[1];
+
+        const shortCommit = commitId.substr(0, 12);
+        patchName = patchName ? `${patchName}.patch` : `cherry-pick-${shortCommit}.patch`;
+        const patchPath = `patches/${patchDirName}`;
+        for (const target of targetBranches) {
+          const branchName = `cherry-pick/${target}/${patchDirName}/${shortCommit}`;
+          d(`fetching electron base branch info for ${target}`);
+          const {
+            data: {
               commit: {
-                tree: { sha: targetBaseTreeSha },
+                sha: targetSha,
+                commit: {
+                  tree: { sha: targetBaseTreeSha },
+                },
               },
             },
-          },
-        } = await octokit.repos.getBranch({
-          owner,
-          repo: 'electron',
-          branch: target,
-        });
-
-        d(`fetching base patch list`);
-        const { data: patchListData } = await octokit.repos
-          .getContent({
+          } = await octokit.repos.getBranch({
             owner,
             repo: 'electron',
-            path: `${patchPath}/.patches`,
-            ref: targetSha,
-          })
-          .catch(err => {
-            console.log(
-              `NOTE: No patches existing for ${patchDirName} in ${target}, added a dir under patches/ but you'll need to manually edit patches/config.json`,
-            );
-            return {
-              data: null,
-            };
+            branch: target,
           });
-        const patchList = patchListData
-          ? Buffer.from(patchListData.content, 'base64').toString('utf8')
-          : '';
-        const newPatchList = patchList + `${patchName}\n`;
 
-        d(`creating tree base_tree=${targetBaseTreeSha}`);
-        const { data: tree } = await octokit.git.createTree({
-          owner,
-          repo: 'electron',
-          base_tree: targetBaseTreeSha,
-          tree: [
-            {
+          d(`fetching base patch list`);
+          const { data: patchListData } = await octokit.repos
+            .getContent({
+              owner,
+              repo: 'electron',
               path: `${patchPath}/.patches`,
-              mode: '100644',
-              type: 'blob',
-              content: newPatchList,
-            },
-            {
-              path: `${patchPath}/${patchName}`,
-              mode: '100644',
-              type: 'blob',
-              content: patch,
-            },
-          ],
-        });
+              ref: targetSha,
+            })
+            .catch(err => {
+              console.log(
+                `NOTE: No patches existing for ${patchDirName} in ${target}, added a dir under patches/ but you'll need to manually edit patches/config.json`,
+              );
+              return {
+                data: null,
+              };
+            });
+          const patchList = patchListData
+            ? Buffer.from(patchListData.content, 'base64').toString('utf8')
+            : '';
+          const newPatchList = patchList + `${patchName}\n`;
 
-        d(`creating commit tree=${tree.sha} parent=${targetSha}`);
-        const { data: commit } = await octokit.git.createCommit({
-          owner,
-          repo: 'electron',
-          tree: tree.sha,
-          parents: [targetSha],
-          message: `chore: cherry-pick ${shortCommit} from ${patchDirName}`,
-        });
+          d(`creating tree base_tree=${targetBaseTreeSha}`);
+          const { data: tree } = await octokit.git.createTree({
+            owner,
+            repo: 'electron',
+            base_tree: targetBaseTreeSha,
+            tree: [
+              {
+                path: `${patchPath}/.patches`,
+                mode: '100644',
+                type: 'blob',
+                content: newPatchList,
+              },
+              {
+                path: `${patchPath}/${patchName}`,
+                mode: '100644',
+                type: 'blob',
+                content: patch,
+              },
+            ],
+          });
 
-        d(`creating ref`);
-        await octokit.git.createRef({
-          owner,
-          repo: 'electron',
-          ref: `refs/heads/${branchName}`,
-          sha: commit.sha,
-        });
+          d(`creating commit tree=${tree.sha} parent=${targetSha}`);
+          const { data: commit } = await octokit.git.createCommit({
+            owner,
+            repo: 'electron',
+            tree: tree.sha,
+            parents: [targetSha],
+            message: message || `chore: cherry-pick ${shortCommit} from ${patchDirName}`,
+          });
 
-        const bugNumber =
-          (/^Bug: (.+)$/m.exec(patch) || [])[1] || (/^Bug= ?chromium:(.+)$/m.exec(patch) || [])[1];
+          d(`creating ref`);
+          await octokit.git.createRef({
+            owner,
+            repo: 'electron',
+            ref: `refs/heads/${branchName}`,
+            sha: commit.sha,
+          });
 
-        const commitMessage = /Subject: \[PATCH\] (.+?)^---$/ms.exec(patch)[1];
+          const bugNumber =
+            (/^Bug: (.+)$/m.exec(patch) || [])[1] ||
+            (/^Bug= ?chromium:(.+)$/m.exec(patch) || [])[1];
 
-        d(`creating pr`);
-        const { data: pr } = await octokit.pulls.create({
-          owner: 'electron',
-          repo: 'electron',
-          head: `${owner}:${branchName}`,
-          base: target,
-          title: `chore: cherry-pick ${shortCommit} from ${patchDirName}`,
-          body: `${commitMessage}\n\nNotes: ${
+          const commitMessage = /Subject: \[PATCH\] (.+?)^---$/ms.exec(patch)[1];
+          const body = `${commitMessage}\n\nNotes: ${
             bugNumber
               ? security
                 ? `Security: backported fix for ${bugNumber}.`
                 : `Backported fix for ${bugNumber}.`
               : `<!-- couldn't find bug number -->`
-          }`,
-          maintainer_can_modify: true,
-        });
+          }`;
 
-        if (!forkUsername) {
-          d(`labelling pr`);
-          await octokit.issues.update({
-            owner,
+          if (interactive) {
+            const repoBaseUrl = 'https://github.com/electron/electron';
+            const comparePath = `${target}...${owner}:${branchName}`;
+            open(
+              `${repoBaseUrl}/compare/${comparePath}?${querystring.stringify({ expand: 1, body })}`,
+            );
+            return;
+          }
+
+          d(`creating pr`);
+          const { data: pr } = await octokit.pulls.create({
+            owner: 'electron',
             repo: 'electron',
-            issue_number: pr.number,
-            labels: [
-              target,
-              'backport-check-skip',
-              'semver/patch',
-              ...(security ? ['security 🔒'] : []),
-            ],
+            head: `${owner}:${branchName}`,
+            base: target,
+            title: `chore: cherry-pick ${shortCommit} from ${patchDirName}`,
+            body,
+            maintainer_can_modify: true,
           });
-        }
 
-        console.log(`Created cherry-pick PR to ${target}: ${pr.html_url}`);
+          if (!forkUsername) {
+            d(`labelling pr`);
+            await octokit.issues.update({
+              owner,
+              repo: 'electron',
+              issue_number: pr.number,
+              labels: [
+                target,
+                'backport-check-skip',
+                'semver/patch',
+                ...(security ? ['security 🔒'] : []),
+              ],
+            });
+          }
+
+          console.log(`Created cherry-pick PR to ${target}: ${pr.html_url}`);
+        }
+      } catch (err) {
+        console.error('Failed to cherry-pick');
+        fatal(err);
       }
-    } catch (err) {
-      console.error('Failed to cherry-pick');
-      fatal(err);
-    }
-  })
+    },
+  )
   .parse(process.argv);
